dc189920d150800018040faad82624d5bdbb595b
ray.chang <yanghaochang1004@gmail.com>
2021-07-27 13:06:53 +0800

    feat(app.py): get and print JWT token

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, request, abort
+from functools import wraps
+
+def get_token_auth_header():
+    if 'Authorization' not in request.headers:
+        abort(401)
+    auth_header = request.headers['Authorization']
+    header_parts = auth_header.split(' ')
+    
+    if len(header_parts) != 2:
+        abort(401)
+    elif header_parts[0].lower() != 'bearer':
+        abort(401)
+    
+    return header_parts[1]
+    
+        
+def requires_auth(f):
+    wraps(f)
+    def wrapper(*args, **kwargs):
+        jwt = get_token_auth_header()
+        return f(jwt, *args, **kwargs)
+    return wrapper
 
 app = Flask(__name__)
 
 @app.route('/')
-def get_token():
-    return 'hello'
+@requires_auth
+def headers(jwt):
+    print(jwt)
+    
+    return 'not implemented'
