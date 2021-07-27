149dac0b49bda46bee619ca520004f82442d9106
ray.chang <yanghaochang1004@gmail.com>
2021-07-27 13:30:33 +0800

    feat(app.py):parse JWT

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, jsonify
 from functools import wraps
+
+import json
+from urllib.request import urlopen
+
+from flask_cors import CORS
+from jose import jwt
+from keys import AUTH0_DOMAIN, API_AUDIENCE ,ALGORITHMS
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
 
 def get_token_auth_header():
     if 'Authorization' not in request.headers:
@@ -14,19 +26,89 @@ def get_token_auth_header():
     
     return header_parts[1]
     
-        
+def verify_jwt(token):
+    jsonurl = urlopen(f"https://{AUTH0_DOMAIN}/.well-known/jwks.json")
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+    
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+            
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+            return payload
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)
+
 def requires_auth(f):
     wraps(f)
     def wrapper(*args, **kwargs):
-        jwt = get_token_auth_header()
-        return f(jwt, *args, **kwargs)
+        token = get_token_auth_header()
+        try:
+            payload = verify_jwt(token)
+        except:
+            abort(401)
+        return f(payload, *args, **kwargs)
     return wrapper
 
-app = Flask(__name__)
+def create_app(test_config=None): 
+    app = Flask(__name__)
+    CORS(app)
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                                'Content-Type, Authorization, true')
+        response.headers.add('Access-Control-Allow-Methods',
+                                'GET, PUT, POST, DELETE, OPTIONS')
+        return response
 
-@app.route('/')
-@requires_auth
-def headers(jwt):
-    print(jwt)
+    @app.route('/authorize')
+    @requires_auth
+    def authorize(payload):
+        print(payload)
+        
+        return jsonify({
+            'status': 200,
+            'message': 'Access Granted'
+        })
     
-    return 'not implemented'
+    return app
